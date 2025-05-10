b399180cccef3b5cf068d0d302567eb5d4cfeb5b
Maichel Sameh <maichelsameh622@gmail.com>
2025-05-10 18:48:50 +0300

    chore: update pubspec.yaml files to use local paths for dependencies and set publish_to to none

--- flutter_keyboard_visibility_web/lib/flutter_keyboard_visibility_web.dart
+++ flutter_keyboard_visibility_web/lib/flutter_keyboard_visibility_web.dart
@@ -1,19 +1,16 @@
-import 'dart:html' as html show window, Navigator;
 import 'package:flutter_keyboard_visibility_platform_interface/flutter_keyboard_visibility_platform_interface.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 /// The web implementation of the [FlutterKeyboardVisibilityPlatform] of the
 /// FlutterKeyboardVisibility plugin.
-class FlutterKeyboardVisibilityPluginWeb
-    extends FlutterKeyboardVisibilityPlatform {
+class FlutterKeyboardVisibilityPluginWeb extends FlutterKeyboardVisibilityPlatform {
   /// Constructs a [FlutterKeyboardVisibilityPluginWeb].
-  FlutterKeyboardVisibilityPluginWeb(html.Navigator navigator);
+  FlutterKeyboardVisibilityPluginWeb();
 
   /// Factory method that initializes the FlutterKeyboardVisibility plugin
   /// platform with an instance of the plugin for the web.
   static void registerWith(Registrar registrar) {
-    FlutterKeyboardVisibilityPlatform.instance =
-        FlutterKeyboardVisibilityPluginWeb(html.window.navigator);
+    FlutterKeyboardVisibilityPlatform.instance = FlutterKeyboardVisibilityPluginWeb();
   }
 
   /// Emits changes to keyboard visibility from the platform. Web is not
